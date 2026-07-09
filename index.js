39ff5ccb6e71a89390a7238b2d065d1cc4d93919
Ruslan <rcernov442@gmail.com>
2026-07-09 17:48:37 +0600

    add Vigenere encode and decode routes

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
-
 const PORT = 8000;
+const password = 'password';
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Server started 8000 port</h1>
+    <h1>Server started</h1>
   `);
+});
+
+app.get('/encode/:text', (req, res) => {
+  const encoded = Vigenere.Cipher(password).crypt(req.params.text);
+
+  res.send(`<h1>${encoded}</h1>`);
+});
+
+app.get('/decode/:text', (req, res) => {
+  const decoded = Vigenere.Decipher(password).crypt(req.params.text);
+
+  res.send(`<h1>${decoded}</h1>`);
 });
 
 app.get('/:text', (req, res) => {
